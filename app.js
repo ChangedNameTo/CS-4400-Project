8792a71cde0aaf6bc88a70f7dfecb3a5dd710f74
ChangedNameTo <william.m95@gmail.com>
2018-04-08 23:08:17 -0400

    Added database stuff

--- app.js
+++ app.js
@@ -20,23 +20,33 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// route setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// database setup
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'will',
+    password : 'testing',
+    database : 'cs4400'
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
