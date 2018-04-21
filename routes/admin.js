68e7ae9f2bff73761161511a6ed252141ea28dfa
Will Mitchell <william.m95@gmail.com>
2018-04-21 16:55:38 -0400

    Admin updating works

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,4 @@
-var express = require('express');
-var router  = express.Router();
+var express = require('express'); var router  = express.Router();
 
 const check            = require('express-validator/check').check;
 const validationResult = require('express-validator/check').validationResult;
@@ -25,7 +24,6 @@ router.get('/confirmed', function(req, res, next) {
         sql     : "SELECT ID,Name,Size,IsCommercial,IsPublic,Street,City,Zip,PropertyType,ApprovedBy,AVG(Rating) AS Rating FROM Property p JOIN Visit v on p.ID = v.PropertyID GROUP BY p.ID ORDER BY p.ID;",
         timeout : 30000 // 30s
     }, function (error, results, fields) {
-        console.log('results',results);
         res.render('admin/confirmed', {results : results});
     });
 });
@@ -53,12 +51,10 @@ router.get('/property/:id', function(req, res, next) {
         // Only grabs one result
         property_result = result[0];
 
-        console.log('result ', property_result);
-
         var has = {};
         // Get has
         connection.query({
-            sql     : "SELECT Name, `IsApproved`, Type FROM `Has` h JOIN `FarmItem` f ON h.`ItemName` = f.Name WHERE h.`PropertyID` = ?;",
+            sql     : "SELECT Name, `IsApproved`, PropertyID, Type FROM `Has` h JOIN `FarmItem` f ON h.`ItemName` = f.Name WHERE h.`PropertyID` = ?;",
             timeout : 30000, // 30s
             values  : req.params.id
         }, function (error, results, fields) {
@@ -118,14 +114,96 @@ router.get('/property/:id', function(req, res, next) {
             better_has['Vegetables'] = vegetables;
             better_has['Nuts']       = nuts;
 
-            console.log(better_has);
-
             // Render the page
             res.render('admin/property', {
                 result : property_result,
                 has    : better_has
             });
         });
+    });
+});
+
+/* Update a property. */
+router.post('/property/:id',[
+    check('name')
+        .isLength({min:1})
+        .withMessage('Name is required.')
+        .trim(),
+    check('street')
+        .isLength({min:1})
+        .withMessage('Street is required.')
+        .trim(),
+    check('city')
+        .isLength({min:1})
+        .withMessage('City is required.')
+        .trim(),
+    check('zip')
+        .isLength({min:1})
+        .withMessage('Zip is required.')
+        .trim(),
+    check('size')
+        .isLength({min:1})
+        .withMessage('Size is required.')
+        .trim()
+], (req, res) => {
+    // Checks for the existance of errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        // FIX ME IF YOU HAVE TIME
+        console.log(errors);
+    }
+    else
+    {
+        console.log(req.body);
+        // Fetch the id
+        req.body.id = req.params.id;
+
+        // Normallize IsPublic and IsCommercial
+        if(req.body.ispublic)
+        {
+            req.body.ispublic = 1;
+        }
+        else
+        {
+            req.body.ispublic = 0;
+        }
+
+        if(req.body.iscommercial)
+        {
+            req.body.iscommercial = 1;
+        }
+        else
+        {
+            req.body.iscommercial = 0;
+        }
+
+        var fields = req.body
+        console.log(fields);
+
+        // Update the property
+        connection.query({
+            sql     : "UPDATE `Property` SET `Name` = ?, `Size` = ?, `IsCommercial` = ?, `IsPublic` = ?, `Street` = ?, `City` = ?, `Zip` = ? WHERE ID = ?;",
+            timeout : 30000, // 30s
+            values  : [fields.name, fields.size, fields.iscommercial, fields.ispublic, fields.street, fields.city, fields.zip, fields.id]
+        }, function (error, results, fields) {
+            res.redirect('/admin/property/' + req.params.id);
+        });
+    }
+});
+
+/* DELETE an item from HAS. */
+router.get('/has/delete/:name-:id', function(req, res, next) {
+    // Get vars
+    var name = req.params.name;
+    var id   = req.params.id;
+
+    connection.query({
+        sql     : "DELETE FROM `Has` WHERE `ItemName` = ? AND `PropertyID` = ?;",
+        timeout : 30000, // 30s
+        values  : [name, id]
+    }, function (error, results, fields) {
+        res.redirect('/admin/property/' + id);
     });
 });
 
